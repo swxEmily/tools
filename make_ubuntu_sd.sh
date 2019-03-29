a1b92d4ebcbccac01a732e2c649bb2cbe50003c2
swxEmily <shiwenxiu@huawei.com>
2019-03-29 14:09:08 +0800

    Title:fix syslog rorate error Description:

--- make_ubuntu_sd.sh
+++ make_ubuntu_sd.sh
@@ -211,7 +211,7 @@ function configure_syslog_and_kernlog()
 #Clean non existent log file entries from status file
 cd /var/lib/logrotate
 test -e status || touch status
-head -l status > status.clean
+head -1 status > status.clean
 sed 's/"//g' status | while read logfile date
 do
     [ -e "\${logfile}" ] && echo "\"\${logfile}\" \${date}"
